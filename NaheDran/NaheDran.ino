d9e94dd60410cb9e7096d7a4f862002631ab7468
Roland Richter <roland.richter@jku.at>
2015-05-28 22:25:01 +0200

    Add example 'NaheDran' for LinuxWochen Linz workshop

---
+++ NaheDran/NaheDran.ino
@@ -0,0 +1,46 @@
+/* NaheDran.ino
+ *
+ * Copyright 2015 Roland Richter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const int QRDPin = A0;
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(QRDPin, INPUT);
+}
+
+
+void loop()
+{
+    int value = analogRead(QRDPin);
+
+    Serial.print("Analoger Wert: "); Serial.println(value);
+
+    delay(100);
+}
+
+/* IDEEN:
+   1. Erweitere das Programm: sobald ein Objekt "zu nahe" am Sensor ist, soll
+   	  die eingebaute LED aufleuchten.
+   2. Zwischen welchen Werten liegt der gemessene Wert? Erweitere das Programm
+      so, dass der Wert in den Bereich [0,100] umgerechnet wird.
+   3*. Bestimme eine Formel, die den gemessenen Wert in die Distanz (in mm)
+      umrechnet.
+*/
