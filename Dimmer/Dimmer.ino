bcfa63b9208e3b834bea0664d68d68564c1c8105
Roland Richter <roland.richter@jku.at>
2015-05-30 23:59:47 +0200

    Change 'Dimmer' to use a photocell (instead of a QRD1114)

--- Dimmer/Dimmer.ino
+++ Dimmer/Dimmer.ino
@@ -17,7 +17,7 @@
  */
 
 // Der analoge Eingangs-Pin des Sensors
-const int QRDPin = A0;
+const int SensorPin = A0;
 
 // Der Ausgangs-Pin der LED
 const int LEDPin = 11;
@@ -25,7 +25,7 @@ const int LEDPin = 11;
 
 void setup()
 {
-    pinMode(QRDPin, INPUT);
+    pinMode(SensorPin, INPUT);
     pinMode(LEDPin, OUTPUT);
 }
 
@@ -34,7 +34,7 @@ void loop()
 {
     // Lies analogen Wert im Bereich [0, 1023] ein, verwandle diesen
     //  in einen Wert im Bereich [0, 255], und gib diesen Wert aus.
-    int value = analogRead(QRDPin);
+    int value = analogRead(SensorPin);
     int pwm = map(value, 0, 1024, 0, 255);
 
     analogWrite(LEDPin, pwm);
@@ -43,6 +43,8 @@ void loop()
 }
 
 /* IDEEN:
-   1. An welche Pins kann eine "dimmbare" LED angeschlossen werden, und an
+   1. Drehe die Logik um: je niedriger die Helligkeit ist, desto heller 
+       soll die LED leuchten.
+   2. An welche Pins kann eine "dimmbare" LED angeschlossen werden, und an
       welche nicht? Warum?
 */
